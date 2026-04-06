d7e796e07461f2162e250de42403cc664b2a90c3
João Guilherme Mendes Cruz <joaomendescruz870@gmail.com>
2026-04-05 21:51:38 -0300

    feat  : Implementação do sessaoRoutes e pacienteRoutes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,16 @@
 import express from 'express';
 import usuarioRoutes from './usuario/usuario.routes';
 import aiRoutes from './ai/ai.routes';
+import sessaoRoutes from './sessao/sessao.routes';
+import pacienteRoutes from './paciente/paciente.routes';
 
 const app = express();
 app.use(express.json());
 
 app.use('/usuarios', usuarioRoutes);
 app.use("/ai", aiRoutes);
+app.use("/sessao",sessaoRoutes);
+app.use("/paciente", pacienteRoutes);
+
 
 export default app;
